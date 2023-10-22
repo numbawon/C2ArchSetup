abc4eda3cea3a037118b53a10fa20b7abfa80b09
Chad Collins <numbawon@gmail.com>
2023-10-21 23:41:09 -0700

    Update btrfs_setup.sh
    Added EFI partition

--- btrfs_setup.sh
+++ btrfs_setup.sh
@@ -21,14 +21,17 @@ DRIVE=${DRIVES[$((DRIVE_NUM-1))]}
 # Unmount the device if it's already mounted
 umount ${DRIVE}*
 
-# Create partition
-echo -e "o\nn\np\n1\n\n\nw" | fdisk ${DRIVE}
+# Create partitions
+echo -e "o\nn\np\n1\n\n+500M\nt\nc\nn\np\n2\n\n\nw" | fdisk ${DRIVE}
 
-# Format the partition with BTRFS
-mkfs.btrfs ${DRIVE}1
+# Format the boot partition with FAT32
+mkfs.fat -F32 ${DRIVE}1
 
-# Mount the partition
-mount ${DRIVE}1 /mnt
+# Format the main partition with BTRFS
+mkfs.btrfs ${DRIVE}2
+
+# Mount the main partition
+mount ${DRIVE}2 /mnt
 
 # Create subvolumes
 btrfs subvolume create /mnt/@
@@ -40,18 +43,22 @@ btrfs subvolume create /mnt/@srv
 btrfs subvolume create /mnt/@opt
 btrfs subvolume create /mnt/@usr_local
 
-# Unmount the partition
+# Unmount the main partition
 umount /mnt
 
 # Remount the subvolumes
-mount -o noatime,compress=lzo,space_cache,subvol=@ ${DRIVE}1 /
-mount -o noatime,compress=lzo,space_cache,subvol=@home ${DRIVE}1 /home
-mount -o noatime,compress=lzo,space_cache,subvol=@snapshots ${DRIVE}1 /.snapshots
-mount -o noatime,compress=lzo,space_cache,subvol=@var ${DRIVE}1 /var
-mount -o noatime,compress=lzo,space_cache,subvol=@tmp ${DRIVE}1 /tmp
-mount -o noatime,compress=lzo,space_cache,subvol=@srv ${DRIVE}1 /srv
-mount -o noatime,compress=lzo,space_cache,subvol=@opt ${DRIVE}1 /opt
-mount -o noatime,compress=lzo,space_cache,subvol=@usr_local ${DRIVE}1 /usr/local
+mount -o noatime,compress=lzo,space_cache,subvol=@ ${DRIVE}2 /
+mount -o noatime,compress=lzo,space_cache,subvol=@home ${DRIVE}2 /home
+mount -o noatime,compress=lzo,space_cache,subvol=@snapshots ${DRIVE}2 /.snapshots
+mount -o noatime,compress=lzo,space_cache,subvol=@var ${DRIVE}2 /var
+mount -o noatime,compress=lzo,space_cache,subvol=@tmp ${DRIVE}2 /tmp
+mount -o noatime,compress=lzo,space_cache,subvol=@srv ${DRIVE}2 /srv
+mount -o noatime,compress=lzo,space_cache,subvol=@opt ${DRIVE}2 /opt
+mount -o noatime,compress=lzo,space_cache,subvol=@usr_local ${DRIVE}2 /usr/local
+
+# Mount the boot partition to /boot/efi (required for UEFI)
+mkdir -p /boot/efi
+mount ${DRIVE}1 /boot/efi
 
 # Create a swapfile
 btrfs subvolume create /mnt/@swap
@@ -63,25 +70,13 @@ chmod 600 /mnt/@swap/swapfile
 mkswap /mnt/@swap/swapfile
 swapon /mnt/@swap/swapfile
 
-echo "${DRIVE}1  /.snapshots     btrfs   noatime,compress=lzo,space_cache,subvol=@snapshots 0 0" >> /etc/fstab
-echo "${DRIVE}1  /.home           btrfs   noatime,compress=lzo,space_cache,subvol=@home 0 0" >> /etc/fstab
-echo "${DRIVE}1  /.var            btrfs   noatime,compress=lzo,space_cache,subvol=@var 0 0" >> /etc/fstab
-echo "${DRIVE}1  /.tmp            btrfs   noatime,compress=lzo,space_cache,subvol=@tmp 0 0" >> /etc/fstab
-echo "${DRIVE}1  /.srv            btrfs   noatime,compress=lzo,space_cache,subvol=@srv 0 0" >> /etc/fstab
-echo "${DRIVE}1  /.opt            btrfs   noatime,compress=lzo,space_cache,subvol=@opt 0 0" >> /etc/fstab
-echo "${DRIVE}1  /.usr/local      btrfs   noatime,compress=lzo,space_cache,subvol=@usr_local 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.snapshots      btrfs   noatime,compress=lzo,space_cache,subvol=@snapshots 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.home           btrfs   noatime,compress=lzo,space_cache,subvol=@home 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.var            btrfs   noatime,compress=lzo,space_cache,subvol=@var 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.tmp            btrfs   noatime,compress=lzo,space_cache,subvol=@tmp 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.srv            btrfs   noatime,compress=lzo,space_cache,subvol=@srv 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.opt            btrfs   noatime,compress=lzo,space_cache,subvol=@opt 0 0" >> /etc/fstab
+echo "${DRIVE}2  /.usr/local      btrfs   noatime,compress=lzo,space_cache,subvol=@usr_local 0 0" >> /etc/fstab
+echo "${DRIVE}1  /boot/           vfat    defaults 0 0" >> /etc/fstab
 echo "/mnt/@swap/swapfile none swap defaults 0 0" >> /etc/fstab
 
-# Check if arch_base script exists
-if [ -f "$(dirname "$0")/arch_base.sh" ]; then
-  read -p "The arch_base.sh script was found in the same directory. Do you want to run it now? (y/n): " RUN_ARCH_BASE
-  if [[ ${RUN_ARCH_BASE,,} == "y" ]]; then
-    bash "$(dirname "$0")/arch_base.sh"
-  else
-    echo "Exiting the script. You can run arch_base manually later."
-    exit 1
-  fi
-else
-  echo "The arch_base script was not found in the same directory. Exiting the script."
-  exit 1
-fi
